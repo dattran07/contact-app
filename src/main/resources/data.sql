3e60d170763348c13956a6bfdef68abfdad515e9
Dat Tran <dattran0724@gmail.com>
2021-04-11 19:37:50 -0400

    correct data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,12 +3,11 @@ INSERT INTO contacts(first_name,middle_name,last_name,street_address,city,state,
 INSERT INTO contacts(first_name,middle_name,last_name,street_address,city,state,zip,email) VALUES ('Amber','Pip','Wands','21906 Southridge Street','Cleveland','Ohio','65133','birons6@harvard.edu');
 INSERT INTO contacts(first_name,middle_name,last_name,street_address,city,state,zip,email) VALUES ('Roseanna','Aland','Wroath','89 Muir Street','Lake Worth','Florida','21413','kthumnel3@independent.co.uk');
 INSERT INTO contacts(first_name,middle_name,last_name,street_address,city,state,zip,email) VALUES ('Abigail','Koo','Thumnel','931 Calypso Center','Aurora','Illinois','77192','gmerwood0@mapquest.com');
-INSERT INTO contacts(first_name,middle_name,last_name,street_address,city,state,zip,email) VALUES ('Lolita','Antoni','Gristhwaite','6291 Di Loreto Park','Salt Lake City','Utah','96592','acalifornia6@sbwire.com');
-INSERT INTO contacts(first_name,middle_name,last_name,street_address,city,state,zip,email) VALUES ('Pamela','Keriann','Langstaff','931 Calypso Center','Charleston','West Virginia', '84219','kjeanesson0@who.int');
+INSERT INTO contacts(first_name,middle_name,last_name,street_address,city,state,zip,email) VALUES ('Lolita','Antoni','Gristhwaite','6291 Di Loreto Park','Reston','Virginia','96592','acalifornia6@sbwire.com');
+INSERT INTO contacts(first_name,middle_name,last_name,street_address,city,state,zip,email) VALUES ('Pamela','Keriann','Langstaff','1325 Chowing Center','Charleston','West Virginia', '84219','kjeanesson0@who.int');
 INSERT INTO contacts(first_name,middle_name,last_name,street_address,city,state,zip,email) VALUES ('Mireielle','Jeanie','Tilbury','58 Pond Lane','Chicago','Illinois','97700','klangstaff8@pagesperso-orange.fr');
 INSERT INTO contacts(first_name,middle_name,last_name,street_address,city,state,zip,email) VALUES ('Burk','Jerrie','Tunna','96385 Rusk Hill','Fort Worth','Texas','32837','ecrock6@nps.gov');
 INSERT INTO contacts(first_name,middle_name,last_name,street_address,city,state,zip,email) VALUES ('Alfonse','Roxanna','Fessler','7223 Packers Hill','Columbia','South Carolina','95372','rfessler8@4shared.com');
-
 
 INSERT INTO contacts_phone(contacts_id,phone_number,phone_type) VALUES (1, '654-234-6521', '0');
 INSERT INTO contacts_phone(contacts_id,phone_number,phone_type) VALUES (1, '345-645-1231', '1');
@@ -19,10 +18,10 @@ INSERT INTO contacts_phone(contacts_id,phone_number,phone_type) VALUES (3, '869-
 INSERT INTO contacts_phone(contacts_id,phone_number,phone_type) VALUES (4, '982-342-1375', '0');
 INSERT INTO contacts_phone(contacts_id,phone_number,phone_type) VALUES (5, '898-685-4567', '1');
 INSERT INTO contacts_phone(contacts_id,phone_number,phone_type) VALUES (6, '568-564-1237', '2');
-INSERT INTO contacts_phone(contacts_id,phone_number,phone_type) VALUES (6, '1567-5635-3256', '1');
+INSERT INTO contacts_phone(contacts_id,phone_number,phone_type) VALUES (6, '157-535-3256', '1');
 INSERT INTO contacts_phone(contacts_id,phone_number,phone_type) VALUES (7, '567-354-9086', '0');
 INSERT INTO contacts_phone(contacts_id,phone_number,phone_type) VALUES (7, '134-645-7584', '1');
 INSERT INTO contacts_phone(contacts_id,phone_number,phone_type) VALUES (8, '234-464-6314', '2');
-INSERT INTO contacts_phone(contacts_id,phone_number,phone_type) VALUES (9, '638-964-346', '2');
+INSERT INTO contacts_phone(contacts_id,phone_number,phone_type) VALUES (9, '638-964-3467', '2');
 INSERT INTO contacts_phone(contacts_id,phone_number,phone_type) VALUES (10, '636-897-0753', '1');
 INSERT INTO contacts_phone(contacts_id,phone_number,phone_type) VALUES (10, '907-564-2465', '0');
